b2cd3334b02a36cc8c380f32aa9abb68cd6927ce
Renato <ren.tito@yahoo.com.br>
2025-09-30 02:02:22 +0000

    project estrutura de dados

--- 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
+++ 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
@@ -1,3 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/messages")
 public class MessageResource {
-    
+
+	@GetMapping("/simpleMessageWelcome")
+	public String simpleMessageWelcome() {
+		return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+	}
 }
