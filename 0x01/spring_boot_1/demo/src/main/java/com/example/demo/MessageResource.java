5556cc21987899bc690d3a7a4545dd01c464602c
Renato <ren.tito@yahoo.com.br>
2025-09-30 02:04:23 +0000

    project estrutura de dados

--- 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
+++ 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/messages")
 public class MessageResource {
 
-	@GetMapping("/simpleMessageWelcome")
-	public String simpleMessageWelcome() {
-		return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
-	}
+    @GetMapping("/simpleMessageWelcome")
+    public String simpleMessageWelcome() {
+        return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+    }
+
+    @GetMapping("/login/{user}/{password}")
+    public String login(@PathVariable String user, @PathVariable String password) {
+        if (user == null || password == null) {
+            return "USUÁRIO E SENHA NÃO INFORMADOS";
+        } else if (user != null && password != null) {
+            return "LOGIN EFETUADO COM SUCESSO !!!";
+        } else {
+            return "USUÁRIO E SENHA INVÁLIDOS";
+        }
+    }
 }
