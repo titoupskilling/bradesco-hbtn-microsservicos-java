559dc5de791cda35597a090556a42adcd28f0521
Renato <ren.tito@yahoo.com.br>
2025-10-01 22:26:54 +0000

    project estrutura de dados

---
+++ 0x02/servicos_API_REST/com/example/demo/Song.java
@@ -0,0 +1,38 @@
+public class Song {
+    private Integer id;
+    private String nome;
+    private String artista;
+    private String album;
+    private String anoLancamento;
+    
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getArtista() {
+        return artista;
+    }
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+    public String getAlbum() {
+        return album;
+    }
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+    public String getAnoLancamento() {
+        return anoLancamento;
+    }
+    public void setAnoLancamento(String anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+}
---
+++ 0x02/servicos_API_REST/com/example/demo/SongController.java
@@ -0,0 +1,36 @@
+@RestController
+   public class SongController {
+
+      @Autowired
+      private SongRepository songRepository;
+
+      @GetMapping("/welcome")
+      public String mensagemBoasVindas() {
+          return "Bem-vindo ao serviço de músicas!";
+      }
+
+      @GetMapping(path = "/allSongs")
+      public List<Song> getAllSongs() {
+         // TODO
+      }
+
+      @GetMapping(path = "/findSong/{id}")
+      public Song findSongById(@PathVariable Integer id) {
+          // TODO
+      }
+
+      @PostMapping(path = "/addSong", consumes = "application/json", produces = "application/json")
+      public Song addSong(@RequestBody Song song) {
+           // TODO
+      }
+
+      @PutMapping(path = "/updateSong", consumes = "application/json", produces = "application/json")
+      public Song updadeSong(@RequestBody Song song) {
+           // TODO
+      }
+
+      @DeleteMapping(path = "/removeSong", consumes = "application/json", produces = "application/json")
+      public void deleteSongById(@RequestBody Song song) {
+           // TODO
+      }
+  }
