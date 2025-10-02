9a5b622d3469d4f1161d3b4e273eebbd61f03499
Renato <ren.tito@yahoo.com.br>
2025-10-02 00:43:06 +0000

    project estrutura de dados

---
+++ 0x04/entities_JPA+H2_database/com/example/jpa_h2_demo/model/Endereco.java
@@ -0,0 +1,53 @@
+public class Endereco {
+    private int id;
+    private String logradouro;
+    private String endereco;
+    private int numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getLogradouro() {
+        return logradouro;
+    }
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+}
